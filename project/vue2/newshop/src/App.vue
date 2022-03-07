e4ea388fbe3be09b8df9e9eff1f7958e52465a1a
addoee <weixvip@gmail.com>
2022-03-07 17:52:20 +0800

    newapp

---
+++ project/vue2/newshop/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    gfdfgfghjkjhjkl
+    jhgfdfgh
+    hgfdfgh
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
